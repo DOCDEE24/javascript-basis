018293bdf2288af63f19cb5dd5cf9249c4ca98b8
DOCDEE24 <oluseyid505@gmail.com>
2025-08-25 19:30:56 +0100

    Temperature Converter

---
+++ TemperatureConverter.js
@@ -0,0 +1,47 @@
+// convert celsiusToFarenheit
+
+function convertCelsiusToFarenheit (temp){
+    let newTemp = temp * (9/5) + 32;
+        return newTemp
+    return(celsius * 9/5) +32;
+}
+
+
+//convert celsiusToKelvin
+
+function convertCelsiusToKelvin(temp){
+    let newTemp = (temp+273.15)
+        return new Temp;
+
+}
+// convert farenheitToCelsius
+
+function convertFarenheitToCelsius (temp){
+    let newTemp = ((temp - 32) * 5/9)
+        return newTemp;
+    return (farenheit - 32) * 5/9;
+}
+
+// convert farenheitToKelvin
+
+function convertFarenheitToKelvin(temp){
+    let newTemp = ((temp -32) * 5/9 + 273.15)
+        return newTemp;
+
+}
+
+
+// convert kelvinToCelsius
+
+function convertKelvinToCelsius(temp){
+    let newTemp = (temp - 273.15)
+        return newTemp;
+}
+
+
+// convert kelvinToFarenheit
+
+function convertKelvinToFarenheit(temp){
+    let newTemp = ((temp - 273.15) * 9/5 + 32) 
+        return newTemp;
+}
